28a571e87d65e794ed71ab1ee76e772590ecca9a
Danilo Morales <danny@gadaproductions.com>
2015-04-29 15:18:23 -0400

    first commit

---
+++ Rails/rails_app/db/migrate/20150428041608_first_migration.rb
@@ -0,0 +1,13 @@
+class FirstMigration < ActiveRecord::Migration
+  
+
+  def self.up
+  	#instructions for changing the db to new state.
+  end
+
+  def self.down
+
+  	#instructions for changing the db back the previous state.
+  	
+  end
+end
